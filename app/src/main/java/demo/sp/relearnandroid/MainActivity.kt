13341bd14c598aa998db511056ccb20458d3910a
wangshaojie <wsj@ytfjr.com>
2019-07-24 08:46:44 +0800

    update

--- app/src/main/java/demo/sp/relearnandroid/MainActivity.kt
+++ app/src/main/java/demo/sp/relearnandroid/MainActivity.kt
@@ -1,9 +1,13 @@
 package demo.sp.relearnandroid
 
+import android.app.LauncherActivity
+import android.app.job.JobInfo
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate
 import android.view.Menu
 import android.view.MenuItem
 import demo.sp.relearnandroid.service.LocalService
@@ -11,6 +15,7 @@ import demo.sp.relearnandroid.service.LocalService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,15 @@ class MainActivity : AppCompatActivity() {
             //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
 
-            startService(Intent(this, LocalService::class.java))
+//            startService(Intent(this, LocalService::class.java))
+
+            var bitmap1 = BitmapFactory.decodeResource(resources, R.mipmap.test)
+            var bitmap2 = BitmapFactory.decodeResource(resources, R.mipmap.test)
+            var bitmap3 = BitmapFactory.decodeResource(resources, R.mipmap.test)
+
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
